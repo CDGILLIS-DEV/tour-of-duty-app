dbc6b608c3261359522d1f725da068a60ac9bcc1
Carl Gillis <darnell@Carls-MacBook-Pro.local>
2025-05-26 15:00:23 -0400

    Added new get request to admin.py route

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Depends
-from app.dependencies.auth import require_role
+from fastapi import APIRouter, Depends, HTTPException, status
+
+from app.dependencies.auth import require_role, get_current_admin
 from app.schemas import UserRoles
 from app.models import User
 
@@ -8,3 +9,8 @@ router = APIRouter
 @router.get("/admin/dashboard")
 def admin_dashboard(current_user: User = Depends(require_role(UserRoles.admin))):
     return {"message": f"Welcome admin {current_user.name}!"}
+
+@router.get("/admin/users")
+def list_all_users(current_user: User = Depends(get_current_admin)):
+    # Logic to return a list of users
+    return {"message": f"Hi, admin {current_user.name}. Here are all users"}
