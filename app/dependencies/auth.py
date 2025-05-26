be1f4087a098080ff2f0033b36f395894f031949
Carl Gillis <darnell@Carls-MacBook-Pro.local>
2025-05-26 14:29:12 -0400

    updated get_current_user logic in auth.py to authenticate JWT

--- app/dependencies/auth.py
+++ app/dependencies/auth.py
@@ -1,28 +1,41 @@
 from passlib.context import CryptContext
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, Security
 from fastapi.security import APIKeyHeader
 from sqlalchemy.orm import Session
+from jose import jwt, JWTError
 
 from app.models import User
 from app.dependencies.db import get_db
 from app.schemas import UserRoles
 from app.utils import verify_password, get_password_hash, create_access_token
+from app.config import SECRET_KEY, ALGORITHM
 
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 api_key_header = APIKeyHeader(name="Authorization", auto_error=False)
 
-
-
-
-def get_current_user(token: str = Depends(api_key_header)):
-    if not token.startswith("Bearer " ):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid token format"    
-        )
-    token_value = token.split(" ")[1] # Proceed to decode and validate JWT
+def get_current_user(
+    token: str = Security(api_key_header),
+    db: Session = Depends(get_db)
+) -> User:
+    credentials_exeption = HTTPException(
+        status_code=401,
+        detail="Invalid or missing credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    try:
+        payload = jwt.decode(token=SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id in None:
+            raise credentials_exeption
+    except JWTError:
+        raise credentials_exeption
+    
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise credentials_exeption
+    
+    return user
 
 def require_role(required_role: UserRoles):
     def role_checker(user: User = Depends(get_current_user)):
